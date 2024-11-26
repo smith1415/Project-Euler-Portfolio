7ffb11cd1d3fe3fd44485a949be5954d892b6b3f
smith1415 <sosmith@students.latinpcs.org>
2024-11-26 10:39:38 -0500

    Update 009-SpecialPythagoreanTriplet.py
    reformat prompt

--- 009-SpecialPythagoreanTriplet.py
+++ 009-SpecialPythagoreanTriplet.py
@@ -1,4 +1,6 @@
-#Problem 9:A Pythagorean triplet is a set of three natural numbers, a<b<c, for which, a^2+b^2=c^2. For example, 3^2+4^2=9+16=25=5^2. There exists exactly one Pythagorean triplet for which a+b+c=1000. Find the product abc.
+#Problem 9:A Pythagorean triplet is a set of three natural numbers, a<b<c, for which, 
+#a^2+b^2=c^2. For example, 3^2+4^2=9+16=25=5^2. There exists exactly one Pythagorean 
+#triplet for which a+b+c=1000. Find the product abc.
 
 num = int(input("What number is the sum of the Pythagorean triplet?"))
 abcList=[]
