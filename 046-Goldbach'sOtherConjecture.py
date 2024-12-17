ece8167b8bee4b66186cbe6f1a757e43f1c3207f
smith1415 <sosmith@students.latinpcs.org>
2024-12-17 11:33:52 -0500

    Update 046-Goldbach'sOtherConjecture.py

--- 046-Goldbach'sOtherConjecture.py
+++ 046-Goldbach'sOtherConjecture.py
@@ -1,4 +1,5 @@
-#Problem 46: It was proposed by Christian Goldbach that every odd composite number can be written as the sum of a prime and twice a square. 
+#Problem 46: It was proposed by Christian Goldbach that every odd composite number can be 
+#written as the sum of a prime and twice a square. 
 """
 9=7+2(1)^2
 15=7+2(2)^2
@@ -7,7 +8,8 @@
 27=19+2(2)^2
 33=31+2(1)^2
 """
-#It turns out that the conjecture was false. What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
+#It turns out that the conjecture was false. What is the smallest odd composite that 
+#cannot be written as the sum of a prime and twice a square?
 def isComposite(num):
     k=0
     while primes[k]<=num**1/2:
