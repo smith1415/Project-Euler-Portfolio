5c6be4c086feda0cc5669da0b8bc1e13c045d8e6
smith1415 <sosmith@students.latinpcs.org>
2024-12-17 11:26:05 -0500

    Update 043-Sub-stringDivisibility.py

--- 043-Sub-stringDivisibility.py
+++ 043-Sub-stringDivisibility.py
@@ -1,4 +1,5 @@
-#Problem 43: The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a rather interesting sub-string divisibility property. Let d<1> be the 1st digit, d<2> be the 2nd digit, and so on. In this way, we note the following: 
+#Problem 43: The number, 1406357289, is a 0 to 9 pandigital number because it is made up of each of the digits 0 to 9 in some order, but it also has a 
+#rather interesting sub-string divisibility property. Let d<1> be the 1st digit, d<2> be the 2nd digit, and so on. In this way, we note the following: 
 """
 d<2>d<3>d<4>=406 is divisible by 2 
 d<3>d<4>d<5>=063 is divisible by 3
