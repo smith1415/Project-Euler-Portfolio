b7d40be7e8cca4d8357b1bad2f511182708923cb
smith1415 <sosmith@students.latinpcs.org>
2024-12-17 11:35:25 -0500

    Update 050-ConsecutivePrimeSum.py

--- 050-ConsecutivePrimeSum.py
+++ 050-ConsecutivePrimeSum.py
@@ -1,4 +1,8 @@
-#Problem 50: The prime 41, can be written as the sum of six consecutive primes: 41=2+3+5+7+11+13. This is the longest sum of consecutive primes that adds to a prime below one-hundred. The longest sum of consecutive primes below one-thousand that adds to a prime, contains 21 terms, and is equal to 953. Which prime, below one-million, can be written as the sum of the most consecutive primes?
+#Problem 50: The prime 41, can be written as the sum of six consecutive primes: 
+#41=2+3+5+7+11+13. This is the longest sum of consecutive primes that adds to a prime 
+#below one-hundred. The longest sum of consecutive primes below one-thousand that adds to 
+#a prime, contains 21 terms, and is equal to 953. Which prime, below one-million, can be 
+#written as the sum of the most consecutive primes?
 
 #This one took me forever
 #it was so difficult for me to quickly find all the primes under a million
