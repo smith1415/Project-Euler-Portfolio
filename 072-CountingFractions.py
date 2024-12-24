ed7f8ebbc480d0eb83f4a3701746aae03b36212c
smith1415 <sosmith@students.latinpcs.org>
2024-12-23 21:06:05 -0500

    Update 072-CountingFractions.py

--- 072-CountingFractions.py
+++ 072-CountingFractions.py
@@ -1,3 +1,4 @@
+#have not solved (yet)
 #Problem 72: Consider the fraction, n/d, where n and d are positive integers. If n<d and 
 #HCF(n,d)=1, it is called a reduced proper fraction. If we list the set of reduced proper 
 #fractions for in ascending order of size, we get:
