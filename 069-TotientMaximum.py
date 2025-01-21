870cfcf4422b20a8619c438ba8aae730596e4f1b
Sophia Smith <sosmith@students.latinpcs.org>
2025-01-21 11:55:29 -0500

    Update 069-TotientMaximum.py

--- 069-TotientMaximum.py
+++ 069-TotientMaximum.py
@@ -1,9 +1,10 @@
-#Problem 69: Euler's totient function, phi(n) [sometimes called the phi function], is defined as 
-#the number of positive integers not exceeding n which are relatively prime to n. For example, as 
-#1, 2, 4, 5, 7, and 8, are all less than or equal to nine and relatively prime to nine, phi(9)=6.
+#Problem 69: Euler's totient function, phi(n) [sometimes called the phi function], is 
+#defined as the number of positive integers not exceeding n which are relatively prime to 
+#n. For example, as 1, 2, 4, 5, 7, and 8, are all less than or equal to nine and 
+#relatively prime to nine, phi(9)=6.
 """
 n Relatively Prime	phi(n) n/phi(n)
-2	1	                1      2
+2	1	              1      2
 3	1,2	              2	     1.5
 4	1,3	              2	     2
 5	1,2,3,4	          4	     1.25
@@ -11,10 +12,10 @@ n Relatively Prime	phi(n) n/phi(n)
 7	1,2,3,4,5,6	      6	     1.1666...
 8	1,3,5,7	          4	     2
 9	1,2,4,5,7,8	      6	     1.5
-10	1,3,7,9	        4	     2.5
+10	1,3,7,9	          4	     2.5
 """
-#It can be seen that n=6 produces a maximum n/phi(n) for n<=10. Find the value of n<=1000000 for 
-#which n/phi(n) is a maximum.
+#It can be seen that n=6 produces a maximum n/phi(n) for n<=10. Find the value of 
+#n<=1000000 for which n/phi(n) is a maximum.
 
 #version 1 (doesn't work):
 """ 
