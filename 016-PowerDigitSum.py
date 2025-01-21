6f36baf1cd7050572b65ce6d92d534f87227c6b6
Sophia Smith <sosmith@students.latinpcs.org>
2025-01-21 09:15:21 -0500

    Update 016-PowerDigitSum.py

--- 016-PowerDigitSum.py
+++ 016-PowerDigitSum.py
@@ -1,4 +1,5 @@
-#Problem 16: 2**15=32768 and the sum of its digits is 3+2+7+6+8=26. What is the sum of #the digits of the number 2**1000?
+#Problem 16: 2**15=32768 and the sum of its digits is 3+2+7+6+8=26. What is the sum of 
+#the digits of the number 2**1000?
 
 num=2**1000
 numstr=str(num)
