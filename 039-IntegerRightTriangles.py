35ae927854e7ce94b0283177bd7a76d6e2bed98c
Sophia Smith <sosmith@students.latinpcs.org>
2025-01-21 11:18:35 -0500

    Update 039-IntegerRightTriangles.py

--- 039-IntegerRightTriangles.py
+++ 039-IntegerRightTriangles.py
@@ -1,6 +1,6 @@
-#Problem 39: If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, 
-#there are exactly three solutions for p=120. {20,48,52}, {24, 45, 51}, {30, 40, 50} For which 
-#value of p<=1000, is the number of solutions maximised?
+#Problem 39: If p is the perimeter of a right angle triangle with integral length sides, 
+#{a,b,c}, there are exactly three solutions for p=120. {20,48,52}, {24, 45, 51}, 
+#{30, 40, 50} For which value of p<=1000, is the number of solutions maximised?
 
 #p<=1000
 #a+b+c<=1000
