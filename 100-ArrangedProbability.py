b1c27aba219cd585875d2f85626e284fa3af2b20
Sophia Smith <sosmith@students.latinpcs.org>
2025-01-21 21:59:45 -0500

    Update 100-ArrangedProbability.py

--- 100-ArrangedProbability.py
+++ 100-ArrangedProbability.py
@@ -1,11 +1,11 @@
 #have not solved (yet)
 #Problem 100: If a box contains twenty-one coloured discs, composed of fifteen blue discs 
-#and six red discs, and two discs were taken at random, it can be seen that the probability 
-#of taking two blue discs, P(BB)=(15/21)*(14/20)=1/2. The next such arrangement, for which 
-#there is exactly 50% chance of taking two blue discs at random, is a box containing eighty-
-#five blue discs and thirty-five red discs. By finding the first arrangement to contain over 
-#10**12=1 000 000 000 000 discs in total, determine the number of blue discs that the box 
-#would contain.
+#and six red discs, and two discs were taken at random, it can be seen that the 
+#probability of taking two blue discs, P(BB)=(15/21)*(14/20)=1/2. The next such 
+#arrangement, for which there is exactly 50% chance of taking two blue discs at random, is 
+#a box containing eighty-five blue discs and thirty-five red discs. By finding the first 
+#arrangement to contain over 10**12=1 000 000 000 000 discs in total, determine the number 
+#of blue discs that the box would contain.
 
 #let x=total num of discs 
 #let y=num of blue discs
@@ -29,4 +29,3 @@ while notFound:
 
 decimals are not exact enough :(
 I need to prime factor"""
-
