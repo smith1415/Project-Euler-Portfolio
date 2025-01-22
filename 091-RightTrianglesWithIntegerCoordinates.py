4900326ef5ff23f5ac2e22a3635005bfc6749a55
Sophia Smith <sosmith@students.latinpcs.org>
2025-01-21 21:45:28 -0500

    Update 091-RightTrianglesWithIntegerCoordinates.py

--- 091-RightTrianglesWithIntegerCoordinates.py
+++ 091-RightTrianglesWithIntegerCoordinates.py
@@ -1,9 +1,10 @@
 #have not solved (yet)
-#Problem 91: The points P(x<1>, y<1>) and Q(x<2>, y<2>) are plotted at integer co-ordinates and 
-#are joined to the origin, O(0,0), to form triOPQ.
+#Problem 91: The points P(x<1>, y<1>) and Q(x<2>, y<2>) are plotted at integer 
+#co-ordinates and are joined to the origin, O(0,0), to form triOPQ.
 #<picture of OPQ>
-#There are exactly fourteen triangles containing a right angle that can be formed when each 
-#co-ordinate lies between 0 and 2 inclusive; that is, 0 <= x<1>, y<1>, x<2>, y<2> <= 2.
+#There are exactly fourteen triangles containing a right angle that can be formed when 
+#each co-ordinate lies between 0 and 2 inclusive; that is, 
+#0 <= x<1>, y<1>, x<2>, y<2> <= 2.
 #<picture of all 14>
 #Given that 0 <= x<1>, y<1>, x<2>, y<2> <= 50, how many right triangles can be formed?
 
